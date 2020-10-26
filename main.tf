88a2ef0a0836850b04a8a858c24f8bf0a3391ace
Manish Sejpal <manishsejpal04@gmail.com>
2020-10-26 08:48:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "helloworld" {
   instance_type = "t2.micro"
 }
 
-resource "aws_instance" "helloworld2" {
+resource "aws_instance" "helloworld23" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 }
